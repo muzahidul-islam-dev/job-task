1c469b8c5b37e4623b20eb294a3d8574050c92b1
Rahat Hossain <rahat1470.com@gmail.com>
2025-12-16 21:24:07 +0600

    feat: add modal component and UI elements
    - Introduced a new Modal component for user interactions.
    - Added Button, Card, and Dialog components for UI consistency.
    - Integrated Radix UI components for dialog and label functionality.
    - Updated globals.css for improved theming and animations.
    - Enhanced page layout with new card structure and modal trigger.
    - Added utility functions for class name management.
    - Updated package.json with new dependencies for UI components.

---
+++ src/components/modal/index.tsx
@@ -0,0 +1,58 @@
+import React, {ReactNode} from 'react';
+import {
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import { Input } from "@/components/ui/input";
+import { Label } from "@radix-ui/react-label";
+import { Button } from '../ui/button';
+
+interface ModalType {
+    children: ReactNode
+}
+
+const Modal: React.FC<ModalType> = ({children}) => {
+    return (
+        <Dialog>
+            <form>
+                <DialogTrigger asChild>
+                    {children}
+                    
+                </DialogTrigger>
+                <DialogContent className="sm:max-w-106.25">
+                    <DialogHeader>
+                        <DialogTitle>Edit profile</DialogTitle>
+                        <DialogDescription>
+                            Make changes to your profile here. Click save when you&apos;re
+                            done.
+                        </DialogDescription>
+                    </DialogHeader>
+                    <div className="grid gap-4">
+                        <div className="grid gap-3">
+                            <Label htmlFor="name-1">Name</Label>
+                            <Input id="name-1" name="name" defaultValue="Pedro Duarte" />
+                        </div>
+                        <div className="grid gap-3">
+                            <Label htmlFor="username-1">Username</Label>
+                            <Input id="username-1" name="username" defaultValue="@peduarte" />
+                        </div>
+                    </div>
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline">Cancel</Button>
+                        </DialogClose>
+                        <Button type="submit">Save changes</Button>
+                    </DialogFooter>
+                </DialogContent>
+            </form>
+        </Dialog>
+    )
+};
+
+export default Modal;
